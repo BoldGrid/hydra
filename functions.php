a831fb48524039cffd53ae3d2ce7cc58f8ec100c
Tim <dev@tim.ph>
2016-05-27 07:30:37 -0400

    update functions.php to empty.

--- functions.php
+++ functions.php
@@ -1,31 +1,8 @@
 <?php
 /**
- * BoldGrid functions and definitions
+ * This is where child-theme specific code should be ran.
  *
- * @package BoldGrid
+ * @package Hydra
  */
-/**
- * Add Theme framework configuration
- */
-require_once get_template_directory(  ) . '/inc/boldgrid-theme-framework-config/config.php';
 
-/**
- * Include the BoldGrid Theme Framework
- */
-require_once get_template_directory(  ) . '/inc/boldgrid-theme-framework/boldgrid-theme-framework.php';
-
-/**
- * Declare WooCommerce.
- */
-add_action( 'after_setup_theme', 'woocommerce_support' );
-function woocommerce_support() {
-    add_theme_support( 'woocommerce' );
-}
-
-
-
-
-
-
-
-
+?>

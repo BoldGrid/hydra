89fa24a83a8b77e96bc73b656948ea1f60dc7130
Tim <dev@tim.ph>
2016-06-07 15:48:16 -0400

    latest in progress revision.

--- header.php
+++ header.php
@@ -16,17 +16,18 @@
 	do_action( 'boldgrid_head_bottom' );
 ?>
 </head>
-<body <?php body_class(); ?> <?php BoldGrid_Framework_Schema::body( true ); ?>>
 
+<body <?php body_class(); ?> <?php BoldGrid_Framework_Schema::body( true ); ?>>
 <?php do_action( 'boldgrid_body_top' ); ?>
 <div id="page" class="hfeed site">
 <?php do_action( 'boldgrid_header_before' ); ?>
+
 	<header id="masthead" class="site-header" role="banner" <?php BoldGrid_Framework_Schema::header( true ); ?>>
-	<?php do_action('boldgrid_menu_secondary'); ?>
-	<?php BoldGrid::skip_link(  ); ?>
-		<div class="container">
+		<?php BoldGrid::skip_link(  ); ?>
 		<?php do_action( 'boldgrid_header_top' ); ?>
-		<?php dynamic_sidebar( 'boldgrid-widget-2' ); ?>
+		<?php do_action('boldgrid_menu_secondary'); ?>
+		<div class="container">
+			<?php dynamic_sidebar( 'boldgrid-widget-2' ); ?>
             <div class="row">
 				<div class="col-md-6">
 					<div class="site-branding">
@@ -38,29 +39,28 @@
         			<?php do_action('boldgrid_menu_social'); ?>
 				</div>
 			</div>
-
 			<div class="row">
         		<div class="col-md-12">
-					<nav id="site-navigation" class="navbar navbar-default col-md-12" role="navigation">
-						<div class="navbar-header">
-							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#primary-navbar">
-								<span class="sr-only">Toggle navigation</span>
-                    			<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-							</button>
-						</div><!-- .navbar-header -->
-					<?php do_action('boldgrid_menu_primary'); ?>
-					</nav><!-- #site-navigation -->
+        			<?php if ( has_nav_menu( 'primary' ) ) : ?>
+						<nav id="site-navigation" class="navbar navbar-default col-md-12" role="navigation">
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#primary-navbar">
+									<span class="sr-only">Toggle navigation</span>
+	                    			<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div>
+							<?php do_action('boldgrid_menu_primary'); ?>
+						</nav>
+					<?php endif; ?>
 				</div>
 			</div>
-			
             <?php do_action( 'boldgrid_header_bottom' ); ?>
 		</div><!-- .container -->
 		<?php do_action('boldgrid_menu_tertiary'); ?>
-	</header><!-- #masthead -->
-    
+	</header><!-- .site-header -->
 	<?php do_action( 'boldgrid_header_after' ); ?>
 	<div id="content" class="site-content">
-    <div class="container-fluid">
-    <div class="row">
+    	<div class="container-fluid">
+    		<div class="row">
